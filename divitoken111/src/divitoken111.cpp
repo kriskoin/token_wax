a7d4e85e3c5e22f6a86e825e335e54789b29e9ff
Kriskoin <cris7775@gmail.com>
2020-11-04 09:33:21 -0600

    Update divitoken111.cpp
    example using simple assets framework

--- divitoken111/src/divitoken111.cpp
+++ divitoken111/src/divitoken111.cpp
@@ -2,3 +2,33 @@
 ACTION divitoken111::test( ) {
    print_f("Tensting on wax \n");
 }
+
+
+
+
+CONTRACT divitoken111 : public eosio::contract{
+public:
+    using contract::contract;
+    ACTION newdivitk() {
+
+        //assign asset attributes
+        name author = get_self();
+        name category = "divi_cat"_n;
+        name owner = "divi_owner"_n;
+        std::string idata = R"json({"name": "Divi Token", "desc" : "Example using simple assets framework" })json";
+        std::string mdata = R"json({"color": "black", "img" : "https://diviproject.org/" })json";
+        bool requireClaim = false;
+
+        //call the simpleassets create action
+        action(
+            { author, "active"_n },
+            "simpleassets"_n,
+            "create"_n,
+            std::tuple(author, category, owner, idata, mdata, requireClaim)
+        )
+        .send();
+
+    }
+};
+
+EOSIO_DISPATCH(divitoken111, (newdivitk))
