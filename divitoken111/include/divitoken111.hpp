3266698dfd2e578ecd54f0b871260a02efd8c934
kriskoin <cris7775@gmail.com>
2020-11-06 06:44:26 +0000

    wip: crate a new NFT

--- divitoken111/include/divitoken111.hpp
+++ divitoken111/include/divitoken111.hpp
@@ -1,11 +1,12 @@
 #include <eosio/eosio.hpp>
 using namespace eosio;
 
-CONTRACT divitoken111 : public contract {
-   public:
-      using contract::contract;
 
-      ACTION test( );
-
-      using test_action = action_wrapper<"test"_n, &divitoken111::test>;
+CONTRACT divitoken111 : public eosio::contract{
+public:
+    using contract::contract;
+   ACTION test();
+   ACTION newdivitk();
 };
+
+EOSIO_DISPATCH(divitoken111, (test)(newdivitk));
--- divitoken111/src/divitoken111.cpp
+++ divitoken111/src/divitoken111.cpp
@@ -3,32 +3,24 @@ ACTION divitoken111::test( ) {
    print_f("Tensting on wax \n");
 }
 
+ACTION divitoken111::newdivitk() {
 
+    //assign asset attributes
+    name author = get_self();
+    name category = "divicat"_n;
+    name owner = "diviowner111"_n;
+    
+    std::string idata = R"json({"name": "Divi Token", "desc" : "Example using simple assets framework" })json";
+    std::string mdata = R"json({"color": "black", "img" : "https://diviproject.org/" })json";
+    bool requireClaim = false;
 
+    //call the simpleassets create action
+    action(
+        { author, "active"_n },
+        "simpleassets"_n,
+        "create"_n,
+        std::tuple(author, category, owner, idata, mdata, requireClaim)
+    )
+    .send();
 
-CONTRACT divitoken111 : public eosio::contract{
-public:
-    using contract::contract;
-    ACTION newdivitk() {
-
-        //assign asset attributes
-        name author = get_self();
-        name category = "divi_cat"_n;
-        name owner = "divi_owner"_n;
-        std::string idata = R"json({"name": "Divi Token", "desc" : "Example using simple assets framework" })json";
-        std::string mdata = R"json({"color": "black", "img" : "https://diviproject.org/" })json";
-        bool requireClaim = false;
-
-        //call the simpleassets create action
-        action(
-            { author, "active"_n },
-            "simpleassets"_n,
-            "create"_n,
-            std::tuple(author, category, owner, idata, mdata, requireClaim)
-        )
-        .send();
-
-    }
-};
-
-EOSIO_DISPATCH(divitoken111, (newdivitk))
+}
